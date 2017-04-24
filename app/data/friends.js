0c806e7baf14617440b1c0ab9853fa9391639c05
Your Name <s.nguyen11@utexas.edu>
2017-04-24 16:53:23 -0500

    changed survey questions

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,19 +1,24 @@
 // data
 var friends = [
   {
-    name: "Ms. Frizzle",
-    image: "https://s-media-cache-ak0.pinimg.com/736x/78/46/59/784659bf763cc29b1655778dd90c11eb.jpg",
-    scores: [1,2,3,4,5,1,2,3,4,5]
+    name: "Gryffindor",
+    image: "https://vignette2.wikia.nocookie.net/harrypotter/images/e/ee/Gryffindor_Crest-0.jpg/revision/latest?cb=20160729145529",
+    scores: [5,3,2,5,1,2,5,2,2,5]
   },
   {
-    name: "Bill Nye",
-    image: "http://www.cincinnatimagazine.com/wp-content/uploads/sites/20/2015/11/Bill-Nye-3-200x300.jpeg",
-    scores: [1,1,1,1,1,1,1,1,1,1]
+    name: "Hufflepuff",
+    image: "https://vignette1.wikia.nocookie.net/harrypotter/images/6/62/Download_%289%29.jpg/revision/latest?cb=20160729145653",
+    scores: [3,1,5,5,2,4,1,1,5,1]
   },
   {
-    name: "Grandpa Bob",
-    image: "http://www.halsteaddiving.com/wordpress/wp-content/uploads/2011/04/Bob.jpg",
-    scores: [2,2,2,2,2,2,2,2,2,2]
+    name: "Ravenclaw",
+    image: "https://vignette4.wikia.nocookie.net/harrypotter/images/2/28/Download_%284%29.jpg/revision/latest?cb=20160729145945",
+    scores: [3,2,1,5,1,5,4,5,2,2]
+  },
+  {
+    name: "Slytherin",
+    image: "https://vignette1.wikia.nocookie.net/harrypotter/images/7/72/Ccd53034-c798-452a-8032-f28e06250d8f.jpg/revision/latest?cb=20160729150848",
+    scores: [1,5,2,5,5,2,5,2,2,2]
   }
 ];
 
